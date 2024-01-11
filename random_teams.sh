f053059b436d02a5f0e6404866c2fdc30c60afd4
Andrew <escapeanaemia@gmail.com>
2024-01-11 21:31:08 +0900

    update

---
+++ random_teams.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+# 33명의 이름이 있는 배열
+names=("Name1" "Name2" "Name3" "Name4" "Name5" "Name6" "Name7" "Name8" "Name9" "Name10" "Name11" "Name12" "Name13" "Name14" "Name15" "Name16" "Name17" "Name18" "Name19" "Name20" "Name21" "Name22" "Name23" "Name24" "Name25" "Name26" "Name27" "Name28" "Name29")
+
+# Fisher-Yates 알고리즘을 사용하여 배열 섞기
+len=${#names[@]}
+for ((i=len-1; i>0; i--)); do
+    j=$((RANDOM % (i+1)))
+    # Swap names[i] and names[j]
+    temp=${names[i]}
+    names[i]=${names[j]}
+    names[j]=$temp
+done
+
+# 조 수
+num_teams=6
+
+# 각 조의 맴버 수 계산
+members_per_team=$(( (len + num_teams - 1) / num_teams ))
+
+# 각 조의 맴버를 문자열로 정리
+team_members_str=""
+for ((i=0; i<num_teams; i++)); do
+    team_members_str+="Team $((i+1)): ${names[@]:i*members_per_team:members_per_team}\n"
+done
+
+# 정리된 문자열 출력
+echo -e "$team_members_str"
